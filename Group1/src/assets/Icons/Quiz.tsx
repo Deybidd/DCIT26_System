a57eae5015d24cb372f79a2e00d6a6a4801bf159
Deybidd <163238128+Deybidd@uses.noreply.github.com>
2025-12-07 05:14:45 +0800

    Admin Dashboard

---
+++ Group1/src/assets/Icons/Quiz.tsx
@@ -0,0 +1,33 @@
+const Quiz = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width={48}
+			height={48}
+			viewBox="0 0 24 24"
+		>
+			<g fill="none" stroke="#000" strokeWidth={1.5}>
+				<path d="M7.5 3.5c-1.556.047-2.483.22-3.125.862c-.879.88-.879 2.295-.879 5.126v6.506c0 2.832 0 4.247.879 5.127C5.253 22 6.668 22 9.496 22h5c2.829 0 4.243 0 5.121-.88c.88-.879.88-2.294.88-5.126V9.488c0-2.83 0-4.246-.88-5.126c-.641-.642-1.569-.815-3.125-.862"></path>
+				<path
+					strokeLinejoin="round"
+					d="M7.496 3.75c0-.966.784-1.75 1.75-1.75h5.5a1.75 1.75 0 1 1 0 3.5h-5.5a1.75 1.75 0 0 1-1.75-1.75Z"
+				></path>
+				<path strokeLinecap="round" d="M6.5 10h4"></path>
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					d="M13.5 11s.5 0 1 1c0 0 1.588-2.5 3-3"
+				></path>
+				<path strokeLinecap="round" d="M6.5 16h4"></path>
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					d="M13.5 17s.5 0 1 1c0 0 1.588-2.5 3-3"
+				></path>
+			</g>
+		</svg>
+	);
+};
+
+export default Quiz;
+
